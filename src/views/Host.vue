04f776d88c060a437ed7de220dab83f9317d4ff3
Kevin Heijboer <kevin.heijboer@gmail.com>
2019-12-02 15:21:32 +0100

    Styling host page

--- src/views/Host.vue
+++ src/views/Host.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
 
-    <div class="code">
-      <h2>Code</h2>
-      <p>{{this.code}}</p>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col">
+          <div class="code">
+            <h2>Code</h2>
+            <p>{{this.code}}</p>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="row d-flex justify-content-center">
+            <card-stack :suit="'s'" :rank="'13'" class="mr-5"></card-stack>
+            <card-deck></card-deck>
+          </div>
+        </div>
+        <div class="col">
+           <p>Player ...'s turn</p>
+        </div>
+      </div>
     </div>
 
-      <div class="row d-flex justify-content-center">
-        <card-stack :suit="'s'" :rank="'13'" class="mr-5"></card-stack>
-        <card-deck></card-deck>
-      </div>
 
   </div>
 </template>
@@ -26,13 +37,19 @@ export default {
   components: {
     CardStack,
     CardDeck
-  }
+  },
+  
 };
 </script>
 
 <style lang="scss">
 .code {
   margin-left: 30px;
+  font-size: 30px;
+  color: white;
+}
+
+p{
   font-size: 30px;
   color: white;
 }
